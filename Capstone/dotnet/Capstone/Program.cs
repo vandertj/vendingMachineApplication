b98b77c982ec82809eec8d7f4c1fd7ef2df45ad5
Elysia Webb <elysia.n.webb@gmail.com>
2021-06-03 12:57:41 -0400

    first thoughts

--- Capstone/dotnet/Capstone/Program.cs
+++ Capstone/dotnet/Capstone/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-
+using System.IO;
+using System.Collections.Generic;
+//C:\Users\Student\workspace\module1-capstone-c-team-1\Example Files\Inventory.txt
 namespace Capstone
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<VendingMachineItem> listOfVendingMachineItems = new List<VendingMachineItem>();
+            string filePath = @"C:\Users\Student\workspace\module1-capstone-c-team-1\Example Files\Inventory.txt";
+            try
+            {
+                using  (StreamReader sr = new StreamReader(filePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        VendingMachineItem vendingMachineItem = new VendingMachineItem(line);
+                        listOfVendingMachineItems.Add(vendingMachineItem);
+                    }
+                    MainMenu.DisplayMainMenu();
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
---
+++ Capstone/dotnet/Capstone/VendingMachineItem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    public class VendingMachineItem
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+        public string Location { get; }
+        public string Type { get; }
+        public int Quantity { get; set; } = 5;
+
+
+        public VendingMachineItem(string vendingItemString)
+        {
+            string[] splitItemString = vendingItemString.Split("|");
+            this.Location = splitItemString[0];
+            this.Name = splitItemString[1];
+            this.Price = decimal.Parse(splitItemString[2]);
+            this.Type = splitItemString[3];
+        }
+        public string PurchaseItem(int purchaseQuantity, decimal inputMoney)
+        {
+            if (inputMoney >= (purchaseQuantity * Price) && purchaseQuantity <= Quantity)
+            {
+                Quantity = Quantity - purchaseQuantity;
+                return "Purchase succesfully completed.";
+            }
+            else if (purchaseQuantity > Quantity)
+            {
+                return "Sorry, product is sold out";
+            }
+            else
+            {
+                return "Insufficient funds, please insert more money";
+            }
+
+        }
+
+        //other methods to calculate change coin amounts & output crunch/yum/whatever
+        public decimal CalculateChange(decimal inputMoney, int purchaseQuantity)
+        {
+            return inputMoney - (Price * purchaseQuantity); 
+        }
+
+        public string OutputPhrase(string itemCode)
+        {
+            if(Type.ToLower() == "chip")
+            {
+                return "Crunch Crunch, Yum!";
+            }else if(Type.ToLower() == "candy") 
+            {
+                return "Munch Munch, Yum!";
+            }else if(Type.ToLower() == "drink")
+            {
+                return "Glug Glug, Yum!";
+            }
+            else
+            {
+                return "Chew Chew, Yum!";
+            }
+        }
+    }
+}
